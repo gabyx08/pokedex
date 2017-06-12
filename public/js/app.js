ab6f3662dad06f001961474357f7ee1df2a7df92
fbk-rena <gaby.ranarz@gmail.com>
2017-06-12 14:15:18 -0500

    modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
-$.getJSON("http://pokeapi.co/api/v2/pokemon/",
-	 function (response) {
-	 var pokemons = response.results;
-	 crearPokemons(pokemons);
-});
+var cargarPagina = function(){
+	obtenerNombresPokemons();
+	$(document).on("click",".pokemon",mostrarInfoPokemon);
+}
+var obtenerNombresPokemons = function(){
+	$.getJSON("http://pokeapi.co/api/v2/pokemon/",
+		 function (response) {
+		 var pokemons = response.results;
+		 crearPokemons(pokemons);
+	});
+};
 function crearPokemons(pokemons) {
 	var $seccion = $("#pokemons");
 	pokemons.forEach(function (pokemon) {
-		var $divContenedor = $("<div/>",{"class":" col-md-3 thumbnail pokemon", "data-url":pokemon.url})
-		var $imagen = $("<img/>",{"src":"https://dummyimage.com/150x150", "alt":"imagenPokemon"})
-		var $div = $("<div />",{"class":"caption"})
+		var url= pokemon.url;
+		url= url.replace("pokemon","pokemon-species");
+		 var $divContenedor = $("<div/>",{"class":"col-sm-3 thumbnail pokemon", "data-url":url, "data-nombre":pokemon.name});
+		 var $imagen = $("<img/>",{"src":"https://dummyimage.com/150x150", "alt":"imagenPokemon"});
+		 var $div = $("<div />",{"class":"caption"});
 	   var $nombre = $('<p/>', {"class":"text-center text-uppercase"});
      $nombre.text(pokemon.name);
 		 $div.append($nombre);
@@ -17,3 +25,46 @@ function crearPokemons(pokemons) {
      $seccion.append($divContenedor);
 	});
 }
+ var plantilla =   '<div class="modal-dialog" role="document">'+
+		 '<div class="modal-content">'+
+			 '<div class="modal-header">'+
+				'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+			 '</div>'+
+			 '<div class="modal-body text-center">'+
+				 '<img src="https://dummyimage.com/150x150" alt="">'+
+				 '<h3 class="text-uppercase">_nombre_</h3>'+
+				 '<p>COLOR: _green_</p>'+
+				 '<p>HABITAT: _grassland_</p>'+
+				 '<p>SHAPE: _cuadruped_</p>'+
+				 '<p>GENERA: _seed_</p>'+
+		 	 '</div>'+
+	 	 '</div>'+
+ 	 '</div>';
+
+var mostrarInfoPokemon = function(){
+	var url = $(this).data("url");
+	var name= $(this).data("nombre");
+console.log(name)
+	$.getJSON(url, function (response) {
+			 var	descripcion={
+				 color:response.color.name,
+				 habitat:response.habitat.name,
+				 shape:response.shape.name,
+				 genera:response.genera[0].genus
+			 }
+		modal(descripcion,name);
+		});
+
+		$('#myModal').modal('show');
+};
+
+var modal= function (descripcion,name){
+	var $modalContenedor = $("#myModal");
+	$modalContenedor.html(plantilla.replace("_green_",descripcion.color)
+.replace("_grassland_",descripcion.habitat)
+.replace("_cuadruped_",descripcion.shape)
+.replace("_seed_",descripcion.genera)
+.replace("_nombre_", name));
+}
+
+$(document).ready(cargarPagina);
